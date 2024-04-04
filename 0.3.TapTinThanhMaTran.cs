b72781651ebc7cf6012b430a576fee67a15ea6be
ltdtpmkt <162813828+ltdtpmkt@users.noreply.github.com>
2024-04-05 02:46:57 +0700

    Update and rename 1.XuLyTapTin.cs to 0.3.TapTinThanhMaTran.cs

---
+++ 0.3.TapTinThanhMaTran.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA_LTDT_PKMT_1
+{
+    internal class TapTinThanhMaTran
+    {       
+        public static void ConvertFileToMatrix(string DuongDanTapTin)
+        {
+            //Chuyển nội dung Tập Tin thành Ma trận
+        }
+    }
+}
